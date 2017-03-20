2405c2e5977e1ca1c2e144b709e45fb4bcf24404
Kirill Korolyov <kirill.korolyov@gmail.com>
2017-03-20 18:02:12 +0000

    Stricter Source validation

--- lib/my_music/library/source.ex
+++ lib/my_music/library/source.ex
@@ -1,6 +1,7 @@
 defmodule MyMusic.Library.Source do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Ecto.UUID
 
   schema "album_sources" do
     field :accurate_rip, :string
@@ -24,12 +25,25 @@ defmodule MyMusic.Library.Source do
       :cue_issues, :discs, :download, :edition, :format, :location, :tag_issues
     ])
     |> validate_inclusion(:location, ~w(apple-music spotify google-music foobar2000))
+    |> validate_required([:location])
+    |> validate_inclusion(:format, ~w(TAK Mixed MPC FLAC MP3 APE))
+    |> validate_inclusion(:discs, 1..100)
+    |> validate_length(:accurate_rip, max: 255)
+    |> validate_length(:comments, max: 255)
+    |> validate_length(:cue_issues, max: 255)
     |> validate_length(:download, max: 1000)
+    |> validate_length(:edition, max: 255)
+    |> validate_length(:tag_issues, max: 255)
 
-    if is_binary(params["mbid"]) do
-      cast(changes, %{mbid: params["mbid"]}, [:mbid])
+    if is_nil(params["mbid"]) do
+      put_change(changes, :mbid, nil)
     else
-      changes
+      case UUID.cast(params["mbid"]) do
+        :error ->
+          add_error(changes, :mbid, "is invalid")
+        {:ok, mbid} ->
+          put_change(changes, :mbid, String.downcase(mbid))
+      end
     end
   end
 end
