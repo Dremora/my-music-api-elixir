c410ab62fa1249a57e26d947c98192be2bb7f526
Kirill Korolyov <kirill.korolyov@gmail.com>
2017-11-10 16:21:57 +0000

    Index with ES on write operations

--- lib/my_music/library/library.ex
+++ lib/my_music/library/library.ex
@@ -1,8 +1,13 @@
 defmodule MyMusic.Library do
   import Ecto.{Query, Changeset}, warn: false
+  import Tirexs.Bulk
   alias MyMusic.Repo
 
   alias MyMusic.Library.Album
+
+  def list_albums do
+    Repo.all(from a in Album, preload: :sources)
+  end
 
   def search(search_string) do
     query = [
@@ -31,10 +36,27 @@ defmodule MyMusic.Library do
     Repo.get(Album, id)
   end
 
+  defp index_album(album) do
+    with {:ok, album} <- album
+    do
+      payload = bulk([ index: "music", type: "album" ]) do
+        index [[
+          id: album.id,
+          artist: album.artist,
+          title: album.title,
+          year: album.year
+        ]]
+      end
+      { :ok, 200, _r } = Tirexs.bump(payload)._bulk()
+    end
+    album
+  end
+
   def create_album(attrs \\ %{}) do
     %Album{}
     |> album_changeset(attrs)
     |> Repo.insert()
+    |> index_album
   end
 
   def update_album(id, attrs) do
@@ -47,13 +69,22 @@ defmodule MyMusic.Library do
         album
         |> album_changeset(attrs)
         |> Repo.update()
+        |> index_album
     end
   end
 
   def delete_album(id) do
     case Repo.get(Album, id) do
       nil -> {:error, "Album not found"}
-      album -> Repo.delete(album)
+      album ->
+        result = Repo.delete(album)
+        payload = bulk([ index: "music", type: "album" ]) do
+          delete [[
+            id: album.id
+          ]]
+        end
+        { :ok, 200, _r } = Tirexs.bump(payload)._bulk()
+        result
     end
   end
 
