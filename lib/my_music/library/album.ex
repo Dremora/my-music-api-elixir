e060b3ffcbc8091a75326563d3533525ac6276c8
Kirill Korolyov <kirill.korolyov@gmail.com>
2018-05-26 20:26:14 +0100

    Add first_played support for JSON import

--- lib/my_music/library/album.ex
+++ lib/my_music/library/album.ex
@@ -31,37 +31,64 @@ defmodule MyMusic.Library.Album do
       |> validate_length(:comments, max: 255)
       |> validate_inclusion(:year, 1900..2100)
 
-    if Map.has_key?(attrs, :first_played) do
-      cond do
-        is_map(attrs.first_played) && Map.has_key?(attrs.first_played, :year) ->
-          date = [
-            Map.get(attrs.first_played, :year),
-            Map.get(attrs.first_played, :month),
-            Map.get(attrs.first_played, :day)
-          ]
+    cond do
+      Map.has_key?(attrs, :first_played) ->
+        cond do
+          is_map(attrs.first_played) && Map.has_key?(attrs.first_played, :year) ->
+            date = [
+              Map.get(attrs.first_played, :year),
+              Map.get(attrs.first_played, :month),
+              Map.get(attrs.first_played, :day)
+            ]
 
-          changes
-          |> put_change(:first_played_timestamp, nil)
-          |> cast(%{first_played_date: date}, [:first_played_date])
-          |> validate_length(:first_played_date, min: 1, max: 3)
+            changes
+            |> put_change(:first_played_timestamp, nil)
+            |> cast(%{first_played_date: date}, [:first_played_date])
+            |> validate_length(:first_played_date, min: 1, max: 3)
 
-        is_map(attrs.first_played) && Map.has_key?(attrs.first_played, :timestamp) ->
-          date = DateTime.from_unix!(attrs.first_played.timestamp, :millisecond)
+          is_map(attrs.first_played) && Map.has_key?(attrs.first_played, :timestamp) ->
+            date = DateTime.from_unix!(attrs.first_played.timestamp, :millisecond)
 
-          changes
-          |> put_change(:first_played_date, nil)
-          |> cast(%{first_played_timestamp: date}, [:first_played_timestamp])
+            changes
+            |> put_change(:first_played_date, nil)
+            |> cast(%{first_played_timestamp: date}, [:first_played_timestamp])
 
-        is_nil(attrs.first_played) ->
-          changes
-          |> put_change(:first_played_date, nil)
-          |> put_change(:first_played_timestamp, nil)
+          is_nil(attrs.first_played) ->
+            changes
+            |> put_change(:first_played_date, nil)
+            |> put_change(:first_played_timestamp, nil)
 
-        true ->
-          add_error(changes, :first_played, "is invalid")
-      end
-    else
-      changes
+          true ->
+            add_error(changes, :first_played, "is invalid")
+        end
+
+      Map.has_key?(attrs, "first_played") ->
+        # JSON import only
+        cond do
+          is_list(attrs["first_played"]) ->
+            changes
+            |> put_change(:first_played_timestamp, nil)
+            |> cast(%{first_played_date: attrs["first_played"]}, [:first_played_date])
+            |> validate_length(:first_played_date, min: 1, max: 3)
+
+          is_integer(attrs["first_played"]) ->
+            date = DateTime.from_unix!(attrs["first_played"], :millisecond)
+
+            changes
+            |> put_change(:first_played_date, nil)
+            |> cast(%{first_played_timestamp: date}, [:first_played_timestamp])
+
+          is_nil(attrs["first_played"]) ->
+            changes
+            |> put_change(:first_played_date, nil)
+            |> put_change(:first_played_timestamp, nil)
+
+          true ->
+            add_error(changes, :first_played, "is invalid")
+        end
+
+      true ->
+        changes
     end
   end
 end
